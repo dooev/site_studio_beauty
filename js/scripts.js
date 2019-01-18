960c8f5941ae68fb38a0612a676f34de167db02a
dooev <dooevdima3@gmail.com>
2019-01-18 17:56:42 +0300

    owl.carousel start template & settings

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(function(){
-
+  
+  //mmenu
   $('#my-menu').mmenu({
     extensions: [ 'widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black'],
     navbar: { title: '<img src="img/logo.png" alt="Салон «Парикмахерская на Южной»"></a>'},
@@ -11,6 +12,27 @@ $(function(){
     $('.hamburger').addClass('is-active');
   }).bind("closed", function(){
     $('.hamburger').removeClass('is-active');
-  })
+  });
+  
+  // services__carousel
+  $('.services__carousel').owlCarousel({
+    loop: true,
+    nav: true,
+    smartSpeed: 700,
+    navText: ['<i class="fa fa-angle-double-left"> <i class="fa fa-angle-double-right">'],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      800: {
+        items: 2
+      },
+      1100: {
+        items: 3
+      }
+    }
+  });
+
 
 });
