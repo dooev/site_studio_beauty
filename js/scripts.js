4554dbb920234c8dfef4d306a5493e3f8fdb92dd
dooev <dooevdima3@gmail.com>
2019-01-28 19:57:47 +0300

    footer & botton UP

--- js/scripts.js
+++ js/scripts.js
@@ -108,4 +108,17 @@ $('select').selectize({
     }
   });
 	//s-partners owl-carousel END
+
+  // button UP START
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > $(this).height()) {
+      $('.top').addClass('active');
+    } else {
+      $('.top').removeClass('active');
+    }
+  });
+  $('.top').click(function(){
+    $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+  })
+  // button UP END
 });
